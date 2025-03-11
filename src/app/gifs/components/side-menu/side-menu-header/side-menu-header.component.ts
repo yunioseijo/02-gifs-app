ef21b779d2af1e0d3fc36235062532c56566c18e
yunioseijo <yunioseijo@gmail.com>
2025-03-11 12:59:21 +0100

    Refactor side menu header to use environment variables for company details and update profile greeting and create alias for route path

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '@env/environment.development';
+
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './side-menu-header.component.html',
 })
-export class SideMenuHeaderComponent { }
+export class SideMenuHeaderComponent {
+  envs = environment
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+};
