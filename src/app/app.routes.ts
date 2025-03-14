91f360cf6960f6cb4521c119322f3a2a2a88636b
yunioseijo <yunioseijo@gmail.com>
2025-03-14 21:38:47 +0100

    Add GIF history page and update side menu for navigation; implement route for search history

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,10 @@ export const routes: Routes = [
         loadComponent: () => import('./gifs/pages/search-page/search-page.component'),
       },
       {
+        path: 'history/:query',
+        loadComponent: () => import('./gifs/pages/gif-history/gif-history.component'),
+      },
+      {
         path: '**',
         redirectTo: 'trending',
       }
---
+++ src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-history',
+  imports: [],
+  templateUrl: './gif-history.component.html',
+})
+export default class GifHistoryComponent { }
