915b111847992f527005913922f78aa6022f48bb
yunioseijo <yunioseijo@gmail.com>
2025-03-17 19:40:30 +0100

    Add query display to GIF history page; inject ActivatedRoute to retrieve search query from route parameters

--- src/app/gifs/pages/gif-history/gif-history.component.ts
+++ src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gif-history',
   imports: [],
   templateUrl: './gif-history.component.html',
 })
-export default class GifHistoryComponent { }
+export default class GifHistoryComponent {
+  //the word 'query' is a parameter in the route, and could be any other word
+  query = toSignal(
+    inject(ActivatedRoute).params.pipe(map(params => params['query'])));
+}
