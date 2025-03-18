eacb08910e60c9a74f1c6e41c52005ee2c13eb82
yunioseijo <yunioseijo@gmail.com>
2025-03-18 18:01:58 +0100

    Implement trending GIF groups for improved display in trending page

--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -22,6 +22,14 @@ export class GifService {
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
+  trendingGifGroups = computed<Gif[][]>(() => {
+    const groups = [];
+    for (let i = 0; i < this.trendingGifs().length; i += 3) {
+      groups.push(this.trendingGifs().slice(i, i + 3));
+    }
+    return groups;
+  });
+
   constructor() {
     this.loadTrendingGifs();
   }
